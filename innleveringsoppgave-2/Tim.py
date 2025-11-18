06467492ba21203f595ff2282d4e6c2d4de9f4f7
Tim Runge <timr@uia.no>
2025-11-18 12:54:55 +0100

    testing

--- innleveringsoppgave-2/Tim.py
+++ innleveringsoppgave-2/Tim.py
@@ -1,3 +1,14 @@
 # giHUBname - timrun15
 print ('does it work?')
 # hallo
+
+print('storytime!')
+print('yes it does!')  
+print('this is fun!')
+print('let us add more lines of code')
+print('adding another line')
+print('final line')
+print('just one more line to go')
+print('this is the last line')
+print('actually, one more for good measure')
+print('now this is the final line for real')
