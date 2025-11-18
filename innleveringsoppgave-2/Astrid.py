fd297ea7d90ccc7bad2eef605b4407fc7f4d174b
Astridesk <astridesk@uia.no>
2025-11-18 16:49:48 +0100

    oppdatert kode

--- innleveringsoppgave-2/Astrid.py
+++ innleveringsoppgave-2/Astrid.py
@@ -1,59 +1,92 @@
+#----------------------------------------------------------------------------------------------------------------
+#Introduksjon
 
- #Introduksjon
 # Beslutningspunkt 1, Silje og Sivert
 # 1A “Erling går til HR -> kompromiss -> Siljes motivasjon svekkes” 
 # 1B “Erling tar individuelle samtaler -> oppfordrer til enighet -> de blir enige” 
 #Beslutningspunkt 2 
-#“Erling tar tidlig dialog -> bedre kommunikasjon -> enighet” 
-#“Erling lar dem ordre på egenhånd -> misnøye fortsetter -> sakskonflikt” 
+#2A “Erling tar tidlig dialog -> bedre kommunikasjon -> enighet” 
+#2B “Erling lar dem ordre på egenhånd -> misnøye fortsetter -> sakskonflikt” 
 #Beslutningspunkt 3
-#“Erling fokuserer på teambygging --> lærer å kommunisere bedre -> prøver å se fra hverandres synspunkt”
-#“Erling fokuserer på fremgang -> jobber ikke effektivt ->prosjektet stagnerer” 
-#Avslutning, 3 mulige utfall
+#3A “Erling fokuserer på teambygging --> lærer å kommunisere bedre -> prøver å se fra hverandres synspunkt”
+#3B “Erling fokuserer på fremgang -> jobber ikke effektivt ->prosjektet stagnerer” 
 
+#Avslutning, 3 mulige utfall
+#----------------------------------------------------------------------------------------------------------------
+
+score = 0
 
 print("Introduksjon")
 
 print("-----------------------------------------------------------------------------------------------------------")
 
-print("\nHvordan håndterer Erling konflikten mellom Silje og Sivert?\n")
+print("\n\033[1mHvordan håndterer Erling konflikten mellom Silje og Sivert?\033[0m \n") #bold tekst: \033[1m tekst \033[0m
 
 print("1A, Går han til HR? \n1B, Eller tar han individuelle samtaler?\n")
 
-#spellproof input
-valg_1 = input("Velg 1A eller 1B  ")
+while True:
+    valg_1 = input("Velg 1A eller 1B  ").strip().upper()
 
-if valg_1 == "1A":
-    print("Erling går til HR. De blir ikke enige, og HR kommer med et kompromiss som de må følge.")
-elif valg_1 == "1B":
-    print("Erling tar individuelle samtaler. Han oppfordrer dem til å bli enige, ellers vil han ta en bestemmelse.")
+    if valg_1 == "1A":
+        print("Erling går til HR. De blir ikke enige, og HR kommer med et kompromiss som de må følge.")
+        score += 0
+        break
+    elif valg_1 == "1B":
+        print("Erling tar individuelle samtaler. Han oppfordrer dem til å bli enige, ellers vil han ta en bestemmelse.")
+        score += 1
+        break
+    else:
+        print("Ugyldig svar. Vennligst velg 1A eller 1B.")
 
 print("-----------------------------------------------------------------------------------------------------------")
-print("\nHvordan håndterer Erling konflikten mellom Hamdi og Jabir\n")
+print("\n\033[1mHvordan håndterer Erling konflikten mellom Hamdi og Jabir?\033[0m \n")
 
 print("2A, Tar han tidlig dialog? \n2B, Eller lar han dem ordne opp på egenhånd?\n")
 
-valg_2 = input("Velg 2A eller 2B  ")
+while True:
+    valg_2 = input("Velg 2A eller 2B  ").strip().upper()
 
-if valg_2 == "2A":
-    print("Hamdi og Jabir begynner å kommunisere bedre. ")
-elif valg_2 == "2B":
-    print(" Misnøyen mellom dem bygger seg opp.")
+    if valg_2 == "2A":
+        print("Erling tar tidlig dialog med dem, og Hamdi og Jabir begynner å kommunisere bedre. ")
+        score += 1
+        break
+    elif valg_2 == "2B":
+        print("Erling lar dem ordne opp selv. Misnøyen mellom dem bygger seg opp.")
+        score -= 1
+        break
+    else:
+        print("Ugyldig svar. Vennligst velg 2A eller 2B.")
 
 print("-----------------------------------------------------------------------------------------------------------")
-print("\nHvordan bevarer Erling motivasjonen i temaet?\n")
+print("\n\033[1mHvordan bevarer Erling motivasjonen i temaet?\033[0m \n")
 
 print("3A, Fokuserer han på teambygging? \n3B, Eller fokuserer han på fremgang?\n")
 
-valg_3 = input("Velg 3A, eller 3B  ")
+while True:
+    valg_3 = input("Velg 3A, eller 3B  ").strip().upper()
 
-if valg_3 == "3A":
-    print("Teammedlemmene får positive opplevelser med hverandre og de lærer å kommunisere bedre.")
-elif valg_3 == "3B":
-    print(" Etter teambyggingen har de ulike medlemmene forbedret relasjonen med sine støttespillere\n" \
-    "og stemningen mellom de uenige forblir antent." \
-    " Dette lover ikke godt for prosjektet.")
+    if valg_3 == "3A":
+        print("Erling fokuserer på teambygging. Teammedlemmene får positive opplevelser med hverandre og de lærer å kommunisere bedre.")
+        score += 1
+        break
+    elif valg_3 == "3B":
+        print("Erling fokuserer på fremgang. Etter teambyggingen har de ulike medlemmene forbedret relasjonen med sine støttespillere\n" \
+        "og stemningen mellom de uenige forblir antent." \
+        " Dette lover ikke godt for prosjektet.")
+        score -= 1
+        break
+    else:
+        print("Ugyldig svar. Vennligst velg 3A eller 3B.")
 
 print("-----------------------------------------------------------------------------------------------------------")
-#Avlutning 3 mulige utfall
+#Avlutning 3 mulige utfall. Lag en score, summen bestemmer utfallene.
 
+#positiv
+if score > 0:
+    print("Teamet gjennompretter tillit og går videre til norming fasen.")
+#nøytral
+elif score == 0:
+    print("En del av konflikten blir løst, men relasjonen er fortsatt svak.")
+#negativ
+else:
+    print("Prosjektet stopper, og felleskapet ryker.")
