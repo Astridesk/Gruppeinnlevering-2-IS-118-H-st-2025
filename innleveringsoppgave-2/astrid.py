7941d7aabd2774345be1a49c732b7375d4a95374
Astridesk <astridesk@uia.no>
2025-11-19 13:24:30 +0100

    oppdatert kode 2

--- innleveringsoppgave-2/astrid.py
+++ innleveringsoppgave-2/astrid.py
@@ -14,9 +14,12 @@
 #Avslutning, 3 mulige utfall
 #----------------------------------------------------------------------------------------------------------------
 
+def wait():
+    input("\n(Trykk Enter for å fortsette...)")
+
 score = 0
 
-print("Introduksjon")
+print("Erling er prosjektleder og trenger din hjelp til å håndtere konflikter i teamet sitt.\n")
 
 print("-----------------------------------------------------------------------------------------------------------")
 
@@ -28,15 +31,17 @@ while True:
     valg_1 = input("Velg 1A eller 1B  ").strip().upper()
 
     if valg_1 == "1A":
-        print("Erling går til HR. De blir ikke enige, og HR kommer med et kompromiss som de må følge.")
+        print("\nErling går til HR. De blir ikke enige, og HR kommer med et kompromiss som de må følge.")
         score += 0
         break
     elif valg_1 == "1B":
-        print("Erling tar individuelle samtaler. Han oppfordrer dem til å bli enige, ellers vil han ta en bestemmelse.")
+        print("\nErling tar individuelle samtaler. Han oppfordrer dem til å bli enige, ellers vil han ta en bestemmelse.")
         score += 1
         break
     else:
-        print("Ugyldig svar. Vennligst velg 1A eller 1B.")
+        print("\nUgyldig svar. Vennligst velg 1A eller 1B.")
+wait()
+
 
 print("-----------------------------------------------------------------------------------------------------------")
 print("\n\033[1mHvordan håndterer Erling konflikten mellom Hamdi og Jabir?\033[0m \n")
@@ -47,15 +52,16 @@ while True:
     valg_2 = input("Velg 2A eller 2B  ").strip().upper()
 
     if valg_2 == "2A":
-        print("Erling tar tidlig dialog med dem, og Hamdi og Jabir begynner å kommunisere bedre. ")
+        print("\nErling tar tidlig dialog med dem, og Hamdi og Jabir begynner å kommunisere bedre. ")
         score += 1
         break
     elif valg_2 == "2B":
-        print("Erling lar dem ordne opp selv. Misnøyen mellom dem bygger seg opp.")
+        print("\nErling lar dem ordne opp selv. Misnøyen mellom dem bygger seg opp.")
         score -= 1
         break
     else:
-        print("Ugyldig svar. Vennligst velg 2A eller 2B.")
+        print("\nUgyldig svar. Vennligst velg 2A eller 2B.")
+wait()
 
 print("-----------------------------------------------------------------------------------------------------------")
 print("\n\033[1mHvordan bevarer Erling motivasjonen i temaet?\033[0m \n")
@@ -66,17 +72,18 @@ while True:
     valg_3 = input("Velg 3A, eller 3B  ").strip().upper()
 
     if valg_3 == "3A":
-        print("Erling fokuserer på teambygging. Teammedlemmene får positive opplevelser med hverandre og de lærer å kommunisere bedre.")
+        print("\nErling fokuserer på teambygging. Teammedlemmene får positive opplevelser med hverandre og de lærer å kommunisere bedre.")
         score += 1
         break
     elif valg_3 == "3B":
-        print("Erling fokuserer på fremgang. Etter teambyggingen har de ulike medlemmene forbedret relasjonen med sine støttespillere\n" \
+        print("\nErling fokuserer på fremgang. Etter teambyggingen har de ulike medlemmene forbedret relasjonen med sine støttespillere\n" \
         "og stemningen mellom de uenige forblir antent." \
         " Dette lover ikke godt for prosjektet.")
         score -= 1
         break
     else:
-        print("Ugyldig svar. Vennligst velg 3A eller 3B.")
+        print("\nUgyldig svar. Vennligst velg 3A eller 3B.")
+wait()
 
 print("-----------------------------------------------------------------------------------------------------------")
 #Avlutning 3 mulige utfall. Lag en score, summen bestemmer utfallene.
@@ -90,3 +97,7 @@ elif score == 0:
 #negativ
 else:
     print("Prosjektet stopper, og felleskapet ryker.")
+
+wait()
+print("-----------------------------------------------------------------------------------------------------------")
+print("\nTakk for at du spilte historien om Erling!\n")
